0457062399f9c5c299ca52f305748962e261123b
Shubham-M-Rathod <96727745+Shubham-M-Rathod@users.noreply.github.com>
2023-02-25 17:48:56 +0530

    Delete my_api directory

--- my_api/routes/user.js
+++
@@ -1,7 +0,0 @@
-const express=require('express')
-const router = express.Router()
-
-const user_controller = require('../controllers/user_controller')
-
-router.post('/new_user', user_controller.new_user);
-module.exports=router;
